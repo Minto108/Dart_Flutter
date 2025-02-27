7e4fd2e75d0d408c31fbf305ee9085e700eed777
Minto108 <158478797+Minto108@users.noreply.github.com>
2025-02-27 22:27:24 +0530

    Add files via upload

---
+++ container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        body: Container(
+          height: 300,
+          width: 300,
+          color: Colors.purple,
+        ),
+      ),
+    );
+  }
+}
