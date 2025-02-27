c2baaf43d50bdf1e7bb9a290f1f67204a3a5623c
Minto108 <158478797+Minto108@users.noreply.github.com>
2025-02-27 22:49:24 +0530

    Add files via upload

---
+++ container_center.dart
@@ -0,0 +1,30 @@
+// rename file as main.dart
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            color: Colors.purple,
+            child: Center(
+              child: Text("Minto"),
+              ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,33 @@
+// rename file as main.dart
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(100),
+            child:  Text("Minto"),
+          ),
+        ),
+      ),
+    );
+  }
+}
